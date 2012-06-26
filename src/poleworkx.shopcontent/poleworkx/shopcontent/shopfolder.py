7186df2fef5d593b12706f63e784d5de3719f689
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-06-27 00:57:49 +0200

    Mojito code: more cleanup for the zopeskel generated dexterity code

--- src/poleworkx.shopcontent/poleworkx/shopcontent/shopfolder.py
+++ src/poleworkx.shopcontent/poleworkx/shopcontent/shopfolder.py
@@ -27,39 +27,13 @@ class IShopFolder(form.Schema, IImageScaleTraversable):
     """
     A sfolderish shop type
     """
-    
-    # If you want a schema-defined interface, delete the form.model
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/shopfolder.xml to define the content type
-    # and add directives here as necessary.
-    
-    form.model("models/shopfolder.xml")
 
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class ShopFolder(dexterity.Container):
     grok.implements(IShopFolder)
-    
-    # Add your class methods and properties here
 
 
-# View class
-# The view will automatically use a similarly named template in
-# shopfolder_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
-class SampleView(grok.View):
+class View(grok.View):
     grok.context(IShopFolder)
     grok.require('zope2.View')
-    
-    # grok.name('view')
+    grok.name('view')
