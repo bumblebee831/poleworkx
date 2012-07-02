3e5a7516d1910abf5c70f623cafce60aad1b1011
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-07-03 01:13:40 +0200

    Provide an add to cart view that uses the cart updater utility

--- src/poleworkx.shopcontent/poleworkx/shopcontent/product.py
+++ src/poleworkx.shopcontent/poleworkx/shopcontent/product.py
@@ -1,22 +1,20 @@
 from five import grok
+from Acquisition import aq_inner
+from AccessControl import Unauthorized
 from plone.directives import dexterity, form
 
 from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
+from zope.component import getUtility
+from zope.component import getMultiAdapter
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
+from plone.namedfile.field import NamedBlobImage
 from plone.app.textfield import RichText
 
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
+from Products.statusmessages.interfaces import IStatusMessage
+from plone.uuid.interfaces import IUUID
+
+from kk.shopified.interfaces import ICartUpdaterUtility
 
 from poleworkx.shopcontent import MessageFactory as _
 
@@ -53,3 +51,28 @@ class View(grok.View):
     grok.context(IProduct)
     grok.require('zope2.View')
     grok.name('view')
+
+    def update(self):
+        context = aq_inner(self.context)
+        if 'form.button.Submit' in self.request:
+            authenticator = getMultiAdapter((context, self.request),
+                                            name=u"authenticator")
+            if not authenticator.verify():
+                raise Unauthorized
+            updater = getUtility(ICartUpdaterUtility)
+            uuid = IUUID(context, None)
+            quantity = self.request.get('quantity', '1')
+            item = updater.add(uuid, quantity)
+            if not item:
+                IStatusMessage(self.request).addStatusMessage(
+                    _(u"Item could not be added to the shopping cart. "
+                      u"Please try again. If the error should persist, please "
+                      u"contact the shop owner"),
+                    type="error")
+                return self.request.response.redirect(context.absolute_url())
+            else:
+                IStatusMessage(self.request).addStatusMessage(
+                    _(u"Item has been added to the shopping cart"),
+                    type="info")
+                return_url = context.absolute_url() + '/@@cart'
+                return self.request.response.redirect(return_url)
