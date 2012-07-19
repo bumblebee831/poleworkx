c85b5e507fd0f6b7d4684dd39f457dd26e956578
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-07-19 17:26:04 +0200

    Pep8/pyflakes cleanup of schema extender
    Made the imports a lot more readable, removed wrong indentation and whitespace usage and introduced ordering on application logic in imports.

--- src/kk.poleworkx_event/kk/poleworkx_event/extender.py
+++ src/kk.poleworkx_event/kk/poleworkx_event/extender.py
@@ -1,82 +1,130 @@
 # -*- coding: utf-8 -*-
 from zope.component import adapts
 from zope.interface import implements
-from archetypes.schemaextender.interfaces import ISchemaExtender, IOrderableSchemaExtender, IBrowserLayerAwareExtender
-from Products.Archetypes.public import ReferenceField, BooleanField, TextField, BooleanWidget, TextAreaWidget, StringField, SelectionWidget, RichWidget, StringWidget
+from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
+from Products.Archetypes.public import StringField, BooleanField, TextField
+from Products.Archetypes.public import StringWidget, BooleanWidget, RichWidget
 from Products.ATContentTypes.interface import IATEvent
 from archetypes.schemaextender.field import ExtensionField
-from Products.ATReferenceBrowserWidget.ATReferenceBrowserWidget import \
-                                         ReferenceBrowserWidget
+
 from kk.poleworkx_event.interfaces import IAddOnInstalled
-     
+
+
 class CourseBlockField(ExtensionField, BooleanField):
     """ course block """
-    
+
     def getCourseBock(self, instance):
         return instance.Schema().getField('courseblock').get(instance)
-         
+
+
 class CourseBlockDetailsField(ExtensionField, TextField):
     """ course block details"""
 
+
 class TotalSeatsField(ExtensionField, StringField):
-    """ total seats"""  
+    """ total seats"""
+
     def getValue(self, instance):
         return instance.Schema().getField('totalseats').get(instance)
+
+
 class BookedSeatsField(ExtensionField, StringField):
-    """ total seats""" 
+    """ total seats"""
+
     def getValue(self, instance):
         return instance.Schema().getField('bookedseats').get(instance)
-class PriceField(ExtensionField, StringField)   :
+
+
+class PriceField(ExtensionField, StringField):
     """ price """
     def getValue(self, instance):
-        return instance.Schema().getField('price').get(instance) 
-class DiscountedPriceField(ExtensionField, StringField)   :
+        return instance.Schema().getField('price').get(instance)
+
+
+class DiscountedPriceField(ExtensionField, StringField):
     """ discounted price """
+
     def getValue(self, instance):
-        return instance.Schema().getField('discountedprice').get(instance)  
-class AvaliableField(ExtensionField, BooleanField)   :
-    """ is registration avaliable """ 
+        return instance.Schema().getField('discountedprice').get(instance)
+
+
+class AvaliableField(ExtensionField, BooleanField):
+    """ is registration avaliable """
+
     def getValue(self, instance):
-        return instance.Schema().getField('avaliable').get(instance)      
+        return instance.Schema().getField('avaliable').get(instance)
+
+
 class EventExtender(object):
     adapts(IATEvent)
     implements(IOrderableSchemaExtender, IBrowserLayerAwareExtender)
     layer = IAddOnInstalled
     fields = [
-       CourseBlockField("courseblock",
-       		languageIndependent = True,
-			default = 0, 
-       		widget = BooleanWidget( label="Kursblock")
-       	),
-       CourseBlockDetailsField("courseblockdetails",
-       		languageIndependent = False,
-       		default_output_type = 'text/x-html-safe',
-
-       		widget = RichWidget( label="Kursblock details" )
-       	), 
-       	TotalSeatsField("totalseats", 
-       		languageIndependent = True,
-       		widget = StringWidget(label = "Teilnehmeranzahl max."),),
-       	BookedSeatsField("bookedseats", 
-       		languageIndependent = True,
-       		widget = StringWidget(label = "Freie Plätze"),),   
-       	PriceField("price", 
-       		languageIndependent = True,
-       		widget = StringWidget(label = "Preis"),),    
-        DiscountedPriceField("discountedprice", 
-       		languageIndependent = True,
-       		widget = StringWidget(label = "Preis discount"),),      
-        AvaliableField("avaliable", 
-       		languageIndependent = True,
-       		default = 0,
-       		widget = BooleanWidget(label = "Anmeldung"), ),         		    		      
-       ]
+       CourseBlockField(
+            "courseblock",
+            languageIndependent=True,
+            default=0,
+            widget=BooleanWidget(
+                label="Kursblock"
+            )
+        ),
+       CourseBlockDetailsField(
+            "courseblockdetails",
+            languageIndependent=False,
+            default_output_type='text/x-html-safe',
+            widget=RichWidget(
+                label="Kursblock details"
+            )
+        ),
+        TotalSeatsField(
+            "totalseats",
+            languageIndependent=True,
+            widget=StringWidget(
+                label="Teilnehmeranzahl max."
+            ),
+        ),
+        BookedSeatsField(
+            "bookedseats",
+            languageIndependent=True,
+            widget=StringWidget(
+                label="Freie Plätze"
+            ),
+        ),
+        PriceField(
+            "price",
+            languageIndependent=True,
+            widget=StringWidget(
+                label="Preis"),
+        ),
+        DiscountedPriceField(
+            "discountedprice",
+            languageIndependent=True,
+            widget=StringWidget(
+                label="Preis discount"
+                ),
+        ),
+        AvaliableField(
+            "avaliable",
+            languageIndependent=True,
+            default=0,
+            widget=BooleanWidget(
+                label="Anmeldung"
+                ),
+            ),
+    ]
 
     def __init__(self, context):
         self.context = context
 
     def getFields(self):
-        return self.fields 
+        return self.fields
+
     def getOrder(self, schematas):
-        schematas['default'] = ['id', 'title', 'description', 'courseblock', 'courseblockdetails', 'location', 'startDate', 'endDate', 'totalseats', 'bookedseats', 'price', 'discountedprice', 'avaliable', 'text', 'attendees', 'eventUrl', 'contactName', 'contactEmail', 'contactPhone']
+        schematas['default'] = ['id', 'title', 'description', 'courseblock',
+                                'courseblockdetails', 'location', 'startDate',
+                                'endDate', 'totalseats', 'bookedseats',
+                                'price', 'discountedprice', 'avaliable',
+                                'text', 'attendees', 'eventUrl', 'contactName',
+                                'contactEmail', 'contactPhone']
         return schematas
